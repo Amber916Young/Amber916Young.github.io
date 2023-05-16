9feb7602bb4fe0e476032f3cb346a5adc4ed985b
tothemoon <57694784+Amber916Young@users.noreply.github.com>
2023-05-16 09:04:08 +0100

    Update testdd.js

--- js/testdd.js
+++ js/testdd.js
@@ -1,5 +1,5 @@
 function openApp() {
-    var appURL = "myapp://myapp/page1";
+    var appURL = "orderitapp://myapp/page1";
     var fallbackURL = "https://apps.apple.com/cn/app/ulysses-writing-app/id1225570693?l=en"; // Redirect to web page if the app is not installed
     window.location.href = appURL;
     setTimeout(function() { // Check if the app is opened after 1 second
